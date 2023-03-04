b7a5ed059db33df599bfa54b8a4a759b6fcd0cf7
minsun0714 <woooooooow22@gmail.com>
2023-03-04 20:33:35 +0900

    🎨 firebaseConfig 파일 분리

--- src/index.tsx
+++ src/index.tsx
@@ -6,16 +6,7 @@ import { getAnalytics } from "firebase/analytics";
 import App from "./App";
 import Join from "./SignUp";
 import Router from "./Router";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyB2mjVx3v_87tJ-lVP2TAVGYCajMyFX9iU",
-  authDomain: "learn-firebase-2ebf6.firebaseapp.com",
-  projectId: "learn-firebase-2ebf6",
-  storageBucket: "learn-firebase-2ebf6.appspot.com",
-  messagingSenderId: "515282344422",
-  appId: "1:515282344422:web:1a1db8b2befc0789a89e27",
-  measurementId: "G-7CTW2049SM",
-};
+import firebaseConfig from "./service/firebase";
 
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);

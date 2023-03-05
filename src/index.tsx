59c534cadf97939716bf847baded0423f785d493
minsun0714 <woooooooow22@gmail.com>
2023-03-05 14:03:56 +0900

    🚚 firebase.ts로 firebaseConfig 이동

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 
-import App from "./App";
-import Join from "./SignUp";
 import Router from "./Router";
-import firebaseConfig from "./service/firebase";
-
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement

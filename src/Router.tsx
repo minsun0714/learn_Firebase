3564aba0c99c9166f6d21198d8d2146365c0531f
minsun0714 <woooooooow22@gmail.com>
2023-03-25 19:09:32 +0900

    🎨 mymemo 페이지 라우팅

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,11 +2,15 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import App from "./App";
 import Join from "./SignUp";
 import MyPage from "./MyPage";
+import MyMemo from "./Memo";
 
 function Router() {
   return (
     <BrowserRouter>
       <Switch>
+        <Route path='/mymemo'>
+          <MyMemo />
+        </Route>
         <Route path='/mypage'>
           <MyPage />
         </Route>
